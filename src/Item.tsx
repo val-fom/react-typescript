cc1ba6c20137f0ddd4b28a4588c2939449adefcc
Valentyn Fomenko <fomenko.vn@gmail.com>
2019-12-02 23:58:11 +0200

    perf(todo-list): use memo

--- src/Item.tsx
+++ src/Item.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Todo } from './TodoList';
 
 export const Item: React.FC<{
   todo: Todo;
   onDelete: (todo: Todo) => void;
   onComplete: (todo: Todo) => void;
-}> = ({ todo, onDelete, onComplete }) => (
-  <li>
-    {todo.name}
-    <button type="button" onClick={(): void => onDelete(todo)}>
-      delete
-    </button>
-    <button type="button" onClick={(): void => onComplete(todo)}>
-      {todo.complete ? 'incomplete' : 'complete'}
-    </button>
-  </li>
-);
+}> = memo(function Item({ todo, onDelete, onComplete }) {
+  return (
+    <li>
+      {todo.name}
+      <button type="button" onClick={(): void => onDelete(todo)}>
+        delete
+      </button>
+      <button type="button" onClick={(): void => onComplete(todo)}>
+        {todo.complete ? 'incomplete' : 'complete'}
+      </button>
+    </li>
+  );
+});
